0ef26d2a55570b43c09a6ad0293d970b8b917b67
Daniel Lucia <dlucia@blackberry.com>
2018-03-01 18:01:44 -0500

    Added header comment

--- datastructures/Chapter2.java
+++ datastructures/Chapter2.java
@@ -162,6 +162,13 @@ class Chapter2 {
     return newNode;
   }
 
+  /**
+    * Given a circular linked list, implement an algorithm which returns node at the beginning
+    * of the loop.
+    * DEFINITION
+    * Circular linked list: A (corrupt) linked list in which a node’s next pointer points to an
+    * earlier node, so as to make a loop in the linked list.
+  **/
   public static Node hasCycle(Node head) {
     Node slowRunner = head;
     Node fastRunner = head;
