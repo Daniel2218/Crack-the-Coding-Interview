f8dcb5ac25491766ac9d9672904994ef7ab34092
Daniel Lucia <dlucia@blackberry.com>
2018-03-01 19:10:44 -0500

    Stripped methods from BinarySearchTree.java and add them to Chapter3.java

--- datastructures/Chapter3.java
+++ datastructures/Chapter3.java
@@ -81,6 +81,61 @@ public class Chapter3 {
   //   VISTED, UNVISTED, VISITING
   // }
 
+  // 4.3
+  public static TreeNode createTree(int sortedArr[]) {
+    int length = sortedArr.length - 1;
+    return createTree(sortedArr, 0, length);
+  }
+
+  private static TreeNode createTree(int sortedArr[], int start, int end) {
+    if(end < start){
+      return null;
+    }
+
+    int midPoint = (start + end) / 2;
+    TreeNode tree = new TreeNode(sortedArr[midPoint]);
+
+    tree.left  = createTree(sortedArr, start, midPoint - 1);
+    tree.right = createTree(sortedArr, midPoint + 1, end);
+
+    return tree;
+  }
+
+  // 4.4
+  public ArrayList<LinkedList<TreeNode>> createLists() {
+    ArrayList<LinkedList<TreeNode>> result = new ArrayList<LinkedList<TreeNode>>();
+    LinkedList<TreeNode> list = new LinkedList<TreeNode>();
+    int level = 0;
+    TreeNode current = this.root;
+
+    list.add(current);
+    result.add(level, list);
+
+    while(true) {
+      for(int i = 0; i < result.get(level).size(); i++) {
+        current =  result.get(level).get(i);
+
+        if(current != null) {
+          if(current.left !=  null) {
+            list.add(current.left);
+          }
+          if(current.right != null) {
+            list.add(current.right);
+          }
+        }
+      }
+
+      if(list.size() > 0) {
+        result.add(level + 1, list);
+      } else {
+        break;
+      }
+      level++;
+    }
+
+    return result;
+  }
+
   public static void main(String args[]) {
     // BinaryTree tree = new BinaryTree();
     // tree.add(5);
@@ -98,5 +153,29 @@ public class Chapter3 {
     // tree.DFS();
     // System.out.println("Pre Order Transversal");
     // tree.preOrder();
+
+    // System.out.println("RUNNING");
+    // BinarySearchTree bst = new BinarySearchTree();
+    // bst.add(1);
+    // bst.add(5);
+    // bst.add(4);
+    // bst.add(-1);
+    // bst.add(6);
+    // bst.add(6);
+    // bst.add(2);
+    // bst.add(-3);
+    // bst.add(0);
+
+    // TreeNode bst;
+    // int arr[] = new int[6];
+    // arr[0] = 1;
+    // arr[1] = 2;
+    // arr[2] = 4;
+    // arr[3] = 5;
+    // arr[4] = 7;
+    // arr[5] = 9;
+    //
+    // bst = createTree(arr);
+    // BTreePrinter.printNode(bst);
   }
 }
